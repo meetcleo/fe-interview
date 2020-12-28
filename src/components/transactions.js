0813e171ab2f1db821a01d3c322608bebbad82b0
Acacia <acacia.mack@fundingcircle.com>
2020-12-28 00:00:47 +0000

    Add transactions component
    This component fetches the data from http://localhost:3002/bills to populate the transactions tab once the component mounts using the useEffect hook. The state of the data is then set and the data that meets the criteria of a transaction is fed into the components that make up each row of the transaction section

---
+++ src/components/transactions.js
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from 'react';
+import { Row, Col } from 'reactstrap';
+
+import { APIConstants } from '.././shared/constants';
+import Icon from '.././shared/components/icon';
+import Accordian from '.././shared/components/accordian';
+import CompanyName from '.././shared/components/companyName';
+
+const Transactions = () => {
+  const [transactions, setTransactions] = useState(null);
+
+  useEffect(() => {
+    const fetchData = () => {
+      fetch(APIConstants.bills)
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => setTransactions(data))
+        .catch((err) => console.log(err));
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      {transactions &&
+        transactions.map((transaction) => {
+          return (
+            <div key={transaction.id}>
+              {transaction.isBill === false && (
+                <>
+                  <Row>
+                    <Col xs="1">
+                      <Icon
+                        iconURL={transaction.iconURL}
+                        altText="business icon"
+                      />
+                    </Col>
+                    <Col xs="3">
+                      <CompanyName companyName={transaction.name} />
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col>
+                      <Accordian
+                        buttonColor="link"
+                        ctaText="Transactions"
+                        items={transaction.transactions}
+                      />
+                    </Col>
+                  </Row>
+                </>
+              )}
+            </div>
+          );
+        })}
+    </>
+  );
+};
+
+export default Transactions;
