6e56eb2b1a75f22e52c89c7e3b4903a8ccd23c86
Joe <joe.a@meetcleo.com>
2021-01-06 15:21:35 +0000

    updated latest create-react-app with typescript template

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import welcomeIcon from './assets/welcome.jpg';
+
+const App: React.FunctionComponent = () => (
+  <img src={welcomeIcon} alt="Welcome!"/>
+);
+
+export default App;
